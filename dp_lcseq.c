113e6f418783bd9ac2c05edf6ac77a279bfc3329
ahsan <ahsan@binbit.net>
2019-02-24 01:41:56 +0600

    updated dp_lcseq.c

--- dp_lcseq.c
+++ dp_lcseq.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
+#include<string.h>
 #define STR_LEN 100
 void LCSeqLen(){
 
 }
 void main(){
-    char strOne[STR_LEN],strTwo[STR_LEN];
+    char X[STR_LEN],Y[STR_LEN];
+    size_t m,n;
     printf("Input String 1:");
+    scanf("%[^\n]%*c", X);
+    printf("Input String 2:");
+    scanf("%[^\n]%*c", Y);
+    m = strlen(X);
+    n = strlen(Y);
+    printf("%s, Length %ld\n%s, Length %ld\n",X,m,Y,n);
 }
