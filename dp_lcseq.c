6228b34d5b2f9a4d3e8fa3c1ce5b9cb4db95ef3a
ahsan <ahsan@binbit.net>
2019-02-24 02:13:34 +0600

    Updates on dp_lcseq.c

--- dp_lcseq.c
+++ dp_lcseq.c
@@ -1,17 +1,30 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 #define STR_LEN 100
-void LCSeqLen(){
-
+void LCSeqLen(char *X, char *Y, size_t m, size_t n, int (*ptr_c)[n],int (*ptr_b)[n])
+{
+    //printf("%s, Length %ld\n%s, Length %ld\n",X,m,Y,n);
+    for (size_t i = 1; i <= m; i++)
+    {
+        for (size_t j = 1; j <= n; j++)
+        {
+            printf("C[%ld][%ld] = %d ", i, j, ptr_c[i][j]);
+        }
+        printf("\n");
+    }
 }
-void main(){
-    char X[STR_LEN],Y[STR_LEN];
-    size_t m,n;
+void main()
+{
+    char X[STR_LEN], Y[STR_LEN];
+    size_t m, n;
     printf("Input String 1:");
     scanf("%[^\n]%*c", X);
     printf("Input String 2:");
     scanf("%[^\n]%*c", Y);
     m = strlen(X);
     n = strlen(Y);
-    printf("%s, Length %ld\n%s, Length %ld\n",X,m,Y,n);
+    int c[m + 1][n + 1],b[m+1][n+1];
+    memset(c, 0, sizeof(int) * (m+1) * (n+1));
+    memset(b, 0, sizeof(int) * (m+1) * (n+1));
+    LCSeqLen(X, Y, m, n, c, b);
 }
