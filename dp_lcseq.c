b74117f283d34e15dd2d598ba06e4f8edd0a207f
ahsan <ahsan@binbit.net>
2019-02-24 05:03:24 +0600

    Finished dp_lcseq.c

--- dp_lcseq.c
+++ dp_lcseq.c
@@ -1,14 +1,131 @@
 #include <stdio.h>
 #include <string.h>
 #define STR_LEN 100
-void LCSeqLen(char *X, char *Y, size_t m, size_t n, int (*ptr_c)[n],int (*ptr_b)[n])
+#define UP 100 //UP ARROW
+#define LF 200 //LEFT ARROW
+#define DI 300 //DIAGONAL ARROW
+int static c[STR_LEN + 1][STR_LEN + 1];
+int static b[STR_LEN + 1][STR_LEN + 1];
+int LCSeqLen(char X[], char Y[], size_t m, size_t n)
 {
     //printf("%s, Length %ld\n%s, Length %ld\n",X,m,Y,n);
+    /*for (int iter = m; iter >= 1; iter--)
+    {
+        X[iter] = X[iter - 1];
+    }
+    for (int iter = n; iter >= 1; iter--)
+    {
+        Y[iter] = Y[iter - 1];
+    }*/
+    for (int a = 0; a <= m; a++)
+    {
+        c[a][0] = 0;
+    }
+    for (int a = 0; a <= n; a++)
+    {
+        c[0][a] = 0;
+    }
     for (size_t i = 1; i <= m; i++)
     {
         for (size_t j = 1; j <= n; j++)
         {
-            printf("C[%ld][%ld] = %d ", i, j, ptr_c[i][j]);
+            if (X[i - 1] == Y[j - 1])
+            {
+                c[i][j] = c[i - 1][j - 1] + 1;
+                b[i][j] = DI;
+                printf("%c %c, %ld %ld\n", X[i - 1], Y[j - 1], i, j);
+            }
+            else
+            {
+                if (c[i - 1][j] >= c[i][j - 1])
+                {
+                    c[i][j] = c[i - 1][j];
+                    b[i][j] = UP;
+                }
+                else
+                {
+                    c[i][j] = c[i][j - 1];
+                    b[i][j] = LF;
+                }
+            }
+        }
+    }
+    return c[m][n];
+}
+void PrintLCSeq(char X[], size_t i, size_t j)
+{
+    if (i == 0 || j == 0)
+    {
+        return;
+    }
+    if (b[i][j] == DI)
+    {
+        PrintLCSeq(X, i - 1, j - 1);
+        printf("%c", X[i-1]);
+    }
+    else if (b[i][j] == UP)
+    {
+        PrintLCSeq(X, i - 1, j);
+    }
+    else
+    {
+        PrintLCSeq(X, i, j - 1);
+    }
+}
+void PrintLCSeqTable(size_t m, size_t n)
+{
+    for (size_t i = 0; i <= m; i++)
+    {
+        for (size_t j = 0; j <= n; j++)
+        {
+            if (b[i][j] == UP)
+            {
+                printf(" U ");
+            }
+            else if (b[i][j] == LF)
+            {
+                /* code */
+                printf(" L ");
+            }
+            else if (b[i][j] == DI)
+            {
+                /* code */
+                printf(" D ");
+            }
+            else
+            {
+                /* code */
+                printf(" %d ", b[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+void PrintLCSeqCostTable(size_t m, size_t n)
+{
+    for (size_t i = 0; i <= m; i++)
+    {
+        for (size_t j = 0; j <= n; j++)
+        {
+            if (c[i][j] == UP)
+            {
+                printf(" U ");
+            }
+            else if (c[i][j] == LF)
+            {
+                /* code */
+                printf(" L ");
+            }
+            else if (c[i][j] == DI)
+            {
+                /* code */
+                printf(" D ");
+            }
+            else
+            {
+                /* code */
+                printf(" %d ", c[i][j]);
+            }
         }
         printf("\n");
     }
@@ -23,8 +140,15 @@ void main()
     scanf("%[^\n]%*c", Y);
     m = strlen(X);
     n = strlen(Y);
-    int c[m + 1][n + 1],b[m+1][n+1];
-    memset(c, 0, sizeof(int) * (m+1) * (n+1));
-    memset(b, 0, sizeof(int) * (m+1) * (n+1));
-    LCSeqLen(X, Y, m, n, c, b);
+    //int c[m+1][n+1], b[m+1][n+1];
+    //memset(c, 0, sizeof(int) * (m+1) * (n+1));
+    //memset(b, 0, sizeof(int) * (m+1) * (n+1));
+    PrintLCSeqTable(m, n);
+    printf("\nLCS Len - %d\n", LCSeqLen(X, Y, m, n));
+
+    PrintLCSeq(X, m, n);
+    printf("\n");
+    PrintLCSeqTable(m, n);
+    printf("\n");
+    PrintLCSeqCostTable(m, n);
 }
