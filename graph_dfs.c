29674fb6ef3211954c273f97fb7f58e2e3c8ecda
anim070 <anim.070@gmail.com>
2019-03-30 13:01:18 +0600

    fixed some minor issues

--- graph_dfs.c
+++ graph_dfs.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+/*int VERTEX_COUNT = 0;
+int *VISITED;*/
 void DFS(int *adjMatPtr, int vertexCount, int *visitedArrPtr, int stratingVertex)
 {
     int j;
@@ -13,6 +16,86 @@ void DFS(int *adjMatPtr, int vertexCount, int *visitedArrPtr, int stratingVertex
         }
     }
 }
+/*int *readAdjMatFromFile(char *fileName)
+{
+    FILE *file = fopen(fileName, "r");
+    char *code;
+    size_t n = 0;
+    int c;
+    if (file == NULL)
+        return NULL; //could not open file
+    fseek(file, 0, SEEK_END);
+    long f_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    code = malloc(f_size);
+    int *ADJ;
+    c = fgetc(file);
+    do
+    {
+        char temp = (char)c;
+
+        if (temp == '\n' || temp == '\r')
+        {
+            code[n++] = '*';
+        }
+        else
+        {
+            code[n++] = temp;
+        }
+    } while ((c = fgetc(file)) != EOF);
+
+    code[n] = '\0';
+    printf("Len:%d\n", n);
+    int i = 0;
+    while (code[i] != '\0')
+    {
+        if (i == 0)
+        {
+            char c[11]; //Assumed digit length not cross 10 (Potential bug)
+            int j = 0;
+            while (code[i] != '*')
+            {
+                c[j++] = code[i++];
+            }
+            c[j] = '\0';
+            VERTEX_COUNT = atoi(c);
+            printf("Ver:%d\n", VERTEX_COUNT);
+            ADJ = malloc(sizeof(int) * VERTEX_COUNT * VERTEX_COUNT);
+            memset(ADJ, 0, sizeof(int) * VERTEX_COUNT * VERTEX_COUNT);
+        }
+
+        for (int k = 0; k < VERTEX_COUNT; k++)
+        {
+            int j = 0;
+            while (code[i] != '*')
+            {
+                if (code[i] == '1')
+                {
+                    printf("C %c ",code[i]);
+                    *(ADJ + k * VERTEX_COUNT + j) = 1;
+                    printf("A %d ",*(ADJ + k * VERTEX_COUNT + j));
+                    j++;
+                    i++;
+                }
+                else if (code[i] == '0')
+                {
+                    printf("C %c ",code[i]);
+                    *(ADJ + k * VERTEX_COUNT + j) = 0;
+                    printf("A %d ",*(ADJ + k * VERTEX_COUNT + j));
+                    j++;
+                    i++;
+                }
+                else
+                {
+                    i++;
+                    continue;
+                }
+            }
+        }
+        i++;
+    }
+    return ADJ;
+}*/
 void printAdjacencyMatrix(int *adjMatPtr, int vertexCount)
 {
     for (short i = 0; i < vertexCount; i++)
@@ -30,7 +113,14 @@ void main(void)
     int vc;
     int ei;
     int flag = 1;
-    printf("Enter Number of Vertex : ");
+    char fileName[40];
+    /*
+    int *adjMat;
+    printf("Enter Adjacency Matrix Name : ");
+    scanf("%s", fileName);
+    adjMat = readAdjMatFromFile(fileName);
+    printAdjacencyMatrix(adjMat, VERTEX_COUNT);*/
+    printf("\nEnter Number of Vertex : ");
     scanf("%d", &vc);
     int VISITED[vc];
     memset(VISITED, 0, sizeof(int) * vc);
